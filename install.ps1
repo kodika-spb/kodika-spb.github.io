6ec48ff36ba74c42bbfe35672bab37409c960e20
Matvey Pashkovskiy <matvey.pashkovskiy@f-secure.com>
2018-12-19 10:50:03 +0200

    changed install file

--- install.ps1
+++ install.ps1
@@ -9,13 +9,8 @@ choco install jdk8 python2 nodejs git androidstudio firefox 7zip sublimetext3 fa
 # - kodu: https://www.microsoft.com/en-us/download/details.aspx?id=10056
 # - stencyl: http://www.stencyl.com/download/get/win/
 # - bifdefender: https://www.bitdefender.com/solutions/free/thank-you.html
-# 
-# http://www.microsoft.com/downloads/details.aspx?familyid=95b251b6-a61d-41b3-b1ec-e7602ab2b48b
-# 0B7N-20MU-Y1RM-PY92-K5YM
+# - stamina: http://stamina.ru/files/StaminaSetup.exe
 #
 # `npm i -g cordova ionic @angular/cli grpc`
 # set ANDROID_HOME
 # accept licenses `%ANDROID_HOME%\tools\bin\sdkmanager --licenses`
-# enable virtualization (VT) in bios
-# install Hyper-V from Windows features
-# `bcdedit /set hypervisorlaunchtype auto`
