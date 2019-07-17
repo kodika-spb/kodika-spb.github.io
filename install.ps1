6c4a26b5d794e88f4ad3de9f8d4070c0c03a5720
mpashkovskiy <mathew.pashkovsky@gmail.com>
2019-07-17 11:01:10 +0300

    updated list of software

--- install.ps1
+++ install.ps1
@@ -1,16 +1,16 @@
-# Set-ExecutionPolicy Unrestricted
-# 
-# Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-choco feature enable --name=allowGlobalConfirmation  --yes=true
-choco install jdk8 python2 nodejs git androidstudio firefox 7zip sublimetext3 far gimp inkscape
+# enabling guest user account
+net user guest /active:yes
 
-# install
-# - adblock plus
+# installing chocolatey and needed software
+Set-ExecutionPolicy Unrestricted
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+# probably restart of PowerShell window is needed here
+choco feature enable --name=allowGlobalConfirmation --yes=true
+choco install python3 nodejs git firefox 7zip sublimetext3 far gimp inkscape arduino vscode
+
+# install manualy
 # - kodu: https://www.microsoft.com/en-us/download/details.aspx?id=10056
 # - stencyl: http://www.stencyl.com/download/get/win/
-# - bifdefender: https://www.bitdefender.com/solutions/free/thank-you.html
 # - stamina: http://stamina.ru/files/StaminaSetup.exe
-#
-# `npm i -g cordova ionic @angular/cli grpc`
-# set ANDROID_HOME
-# accept licenses `%ANDROID_HOME%\tools\bin\sdkmanager --licenses`
+# - bifdefender: https://www.bitdefender.com/solutions/free/thank-you.html
+# - adblock plus extension to firefox (also login as guest and install it there)
